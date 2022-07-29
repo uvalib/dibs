726bcb8e7e238a97414e9a59db88212ae54dbeb5
Dave Goldstein <dpg3k@virginia.edu>
2022-07-29 14:56:28 -0400

    Add Jeremy Wilk as DIBS admin

--- package/scripts/add-admins.sh
+++ package/scripts/add-admins.sh
@@ -7,6 +7,7 @@ admin/people-manager add role="library" uname="cm9kg"   # Caleb Mechem
 admin/people-manager add role="library" uname="cma4u"   # Carla Arton
 admin/people-manager add role="library" uname="dpg3k"   # Dave Goldstein
 admin/people-manager add role="library" uname="jlk4p"   # Jack Kelly
+admin/people-manager add role="library" uname="jw6t"    # Jeremy Wilk
 admin/people-manager add role="library" uname="md5wz"   # Mike Durbin
 admin/people-manager add role="library" uname="mhw8m"   # Mark Witteman
 admin/people-manager add role="library" uname="naw4t"   # Nestor Walker
